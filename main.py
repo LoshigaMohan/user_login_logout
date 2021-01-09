dd24738d16707890318b122f72980950ed460d57
loshiga mohan <loshiga.mohan@student.griffith.ie>
2021-01-09 23:56:55 +0530

    Bracket 1 Completed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from google.appengine.ext import ndb
 import os
 
 from myuser import MyUser
+from directory import Directory
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,7 +15,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 )
 
 class MainPage(webapp2.RequestHandler):
-    
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         url = ''
@@ -25,15 +26,29 @@ class MainPage(webapp2.RequestHandler):
             url_string = 'logout'
             myuser_key = ndb.Key('MyUser', user.user_id())
             myuser = myuser_key.get()
+
+            #First Time Login
             if myuser == None:
-                welcome = 'Welcome to application'
+                welcome = 'Welcome to the application'
                 myuser = MyUser(id=user.user_id())
-                myuser.put()
 
                 #Create Root Directory of the user
-                path = os.getcwd()
-                self.response.write("The current working directory is %s" % path)
-                os.mkdir('hello')
+                directory_id = myuser.key.id() + '/'
+                directory = Directory(id=directory_id)
+
+                directory.parent_directory = None
+                directory.name = 'root'
+                directory.path = '/'
+                directory.put()
+
+                myuser.root_directory = directory.key
+                myuser.put()
+
+                # set current path on first login to root directory
+                myuser.current_directory = ndb.Key(Directory, myuser.key.id() + '/')
+
+                myuser.put()
+
 
         else:
             url = users.create_login_url('/')
@@ -46,7 +61,7 @@ class MainPage(webapp2.RequestHandler):
             'welcome' : welcome
         }
         template = JINJA_ENVIRONMENT.get_template('main.html')
-        self.response.write(template.render(template_values))   
+        self.response.write(template.render(template_values))
 
 # starts the web application we specify the full routing table here as well
 app = webapp2.WSGIApplication([
